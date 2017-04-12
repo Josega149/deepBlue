69edab6e145c0d4102085e2258f6609d919f96b6
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-12 00:28:47 -0500

    navbar complete

---
+++ imports/client/Navigbar.jsx
@@ -0,0 +1,47 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export default class Navigbar extends Component {
+
+  render() {
+    return (
+      <div>
+                <Navbar className="navbar-inverse">
+                    <Navbar.Header>
+                      <LinkContainer to="/">
+                        <Navbar.Brand className="head bold" >
+                            Azul Profundo
+                        </Navbar.Brand>
+                      </LinkContainer>
+                        <Navbar.Toggle/>
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                            <Nav className="bod">
+                              <NavDropdown  title="Colombia" id="Colombia">
+                                <LinkContainer to="/map">
+                                  <MenuItem >Mapa</MenuItem>
+                                </LinkContainer>
+                                <LinkContainer to="/info">
+                                  <MenuItem >Info</MenuItem>
+                                </LinkContainer>
+                              </NavDropdown>
+                              <NavDropdown  title="Mundo" id="Mundo">
+                                <LinkContainer to="/map">
+                                  <MenuItem >Mapa</MenuItem>
+                                </LinkContainer>
+                                <LinkContainer to="/info">
+                                  <MenuItem >Info</MenuItem>
+                                </LinkContainer>
+                              </NavDropdown>
+
+                            </Nav>
+                        <Nav pullRight>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+    );
+  }
+}
