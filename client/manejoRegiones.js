633b99c96d1a9595af1e881f920f7de0ac9229f0
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-13 01:14:38 -0500

    funcionan los marcadores perfecto

--- client/manejoRegiones.js
+++ client/manejoRegiones.js
@@ -1,38 +1,41 @@
 $(document).ready(function() {
 
-    var mapData = {
-        "CO-RAN": 1135,
-        "CO-RCA": 200,
-        "CO-BOG": 200
-
-    };
-
     var pozoSelecionado = undefined;
-    var mapaPozos = {};
-
+    var sensor = {
+        _id: 0,
+        avgTemp: 20,
+        avgPh: 8,
+        avgSalt: 4,
+        lat: 6.544560,
+        lon: -78.019409,
+        lastTemp: 40,
+        lastPh: 8,
+        lastSal: 4
+    };
+    var msg = [sensor];
     var convert = [];
-    msg = [];
+    console.log(msg);
     for (var i = 0; i < msg.length; i++) {
-        mapaPozos[msg[i].id] = msg[i];
         var color = undefined;
-        if (msg[i].estado === "PRODUCCION") {
+        if (msg[i].lastTemp > 30) {
             color = "green";
-        } else if (msg[i].estado === "PARADO") {
+        } else if (msg[i].lastTemp === 30) {
             color = "red";
-        } else if (msg[i].estado === "CLAUSURADO") {
+        } else if (msg[i].lastTemp === 30) {
             color = "black";
         }
         var n = {
             latLng: [
                 msg[i].lat, msg[i].lon
             ],
-            name: msg[i].id,
+            name: msg[i]._id,
             style: {
                 fill: color
             }
         };
         convert.push(n);
     }
+    console.log(convert);
 
     $('#colombia-map').vectorMap({
         map: 'co_mill',
@@ -46,64 +49,43 @@ $(document).ready(function() {
                 "stroke-opacity": 0
             }
         },
-        series: {
-            regions: [
-                {
-                    values: mapData,
-                    scale: [
-                        "#1ab394", "#22d6b1"
-                    ],
-                    normalizeFunction: 'polynomial'
-                }
-            ]
+        markers: convert,
+        onMarkerClick: function(event, index) {
+            pozoSelecionado = map.params.main.markers[index].name;
+            var pozo = mapaPozos[pozoSelecionado];
+            $('#infoPozo').text("pozo: " + pozoSelecionado);
+            $.get("/registro/ener/" + pozo.id, function(data, status) {
+                console.log(data.info);
+                $("#ener").text(data.info);
+            });
+            $.ajax({
+                method: "GET",
+                url: "/registro / ener / " + pozo.id
+            }).done(function(msg) {
+                console.log(msg["info"]);
+                $("#ener").text(msg["info"]);
+            });
+            $.ajax({
+                method: "GET",
+                url: "/registro/temp/" + pozo.id
+            }).done(function(msg) {
+                console.log(msg["info"]);
+                $("#temp").text(msg["info"]);
+            });
+            $.ajax({
+                method: "GET",
+                url: "/registro/barr/" + pozo.id
+            }).done(function(msg) {
+                console.log(msg["info"]);
+                $("#barr").text(msg["info"]);
+            });
+            console.log(pozoSelecionado);
+        },
+        markerStyle: {
+            initial: {
+                fill: '#F8E23B',
+                stroke: '#383f47'
+            }
         }
     });
 });
-
-
-/**
-,
-mapUrlByCode: function(code, multiMap) {
-    // var id = code.toLowerCase()+'_merc';
-    // return id;
-    // console.log("CODIGO REGION "+code);
-    var path = 'js/plugins/jvectormap/col-compl.js';
-    //console.log(path);
-    var reg = {
-        'CO-RCA': "Caribe",
-        'CO-RAN': "Andina",
-        'CO-RAM': "Amazonia",
-        'CO-RPA': "Pacifico",
-        'CO-ROR': "Orinoquia",
-        'CO-COMPL': "Nacional"
-    };
-    var stringP = $('#title-h').text();
-    // console.log(stringP);
-    string2 = stringP.split(":");
-    //console.log(string2[0]);
-    // var cuantos = cuantosPozos(reg[code].toUpperCase());
-    if (reg[code] == undefined) {
-        //no hace nada
-    } else {
-
-        stringP = string2[0].concat(": ", reg[code]);
-        //console.log(stringP);
-        var stringP = $('#title-h').text(stringP);
-
-        var estadoPozosAntes = $('#estadoPozos').text();
-        estadoPozosAntes = estadoPozosAntes.split(":")[0];
-        $('#estadoPozos').text(estadoPozosAntes + ": " + reg[code]);
-
-        var estadoEmergenciasPerc = $('#emergenciasPerc').text();
-        estadoEmergenciasPerc = estadoEmergenciasPerc.split(":")[0];
-        $('#emergenciasPerc').text(estadoEmergenciasPerc + ": " + reg[code]);
-
-        var estadoListaEmergen = $('#listaEmergen').text();
-        estadoListaEmergen = estadoListaEmergen.split(":")[0];
-        $('#listaEmergen').text(estadoListaEmergen + ": " + reg[code]);
-
-        cuantosPozos(reg[code].toUpperCase());
-
-    }
-    return path;
-}*/
