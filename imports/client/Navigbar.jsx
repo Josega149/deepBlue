37ef7c8c0715fedc96fc081e91b81849d70908d4
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-05-02 20:45:44 -0500

    cambios navbar

--- imports/client/Navigbar.jsx
+++ imports/client/Navigbar.jsx
@@ -8,23 +8,43 @@ export default class Navigbar extends Component {
   render() {
     return (
       <div>
-                <Navbar className="navbar-inverse">
+                <Navbar className="navbar centerNav">
+                  <Navbar.Collapse >
+                    <Nav className="bod">
+                      <NavDropdown  title="Informate" >
+                        <LinkContainer to="/info">
+                          <MenuItem >Problematica</MenuItem>
+                        </LinkContainer>
+                        <LinkContainer to="/info">
+                          <MenuItem >¿Que hacer?</MenuItem>
+                        </LinkContainer>
+                      </NavDropdown>
+                      <NavDropdown  title="Contacto" >
+                        <LinkContainer to="/info">
+                          <MenuItem >Nosotros</MenuItem>
+                        </LinkContainer>
+                        <LinkContainer to="/info">
+                          <MenuItem >Como contribuir</MenuItem>
+                        </LinkContainer>
+                      </NavDropdown>
+
+                    </Nav>
+                      <Nav pullRight>
+                      </Nav>
                     <Navbar.Header>
                       <LinkContainer to="/">
-                        <Navbar.Brand className="head bold" >
-                            Azul Profundo
+                        <Navbar.Brand className="imagenCoralCare" >
                         </Navbar.Brand>
                       </LinkContainer>
                         <Navbar.Toggle/>
                     </Navbar.Header>
-                    <Navbar.Collapse>
                             <Nav className="bod">
                               <NavDropdown  title="Colombia" id="Colombia">
                                 <LinkContainer to="/map">
                                   <MenuItem >Mapa</MenuItem>
                                 </LinkContainer>
                                 <LinkContainer to="/info">
-                                  <MenuItem >Info</MenuItem>
+                                  <MenuItem >Estadisticas</MenuItem>
                                 </LinkContainer>
                               </NavDropdown>
                               <NavDropdown  title="Mundo" id="Mundo">
@@ -32,7 +52,7 @@ export default class Navigbar extends Component {
                                   <MenuItem >Mapa</MenuItem>
                                 </LinkContainer>
                                 <LinkContainer to="/info">
-                                  <MenuItem >Info</MenuItem>
+                                  <MenuItem >Estadisticas</MenuItem>
                                 </LinkContainer>
                               </NavDropdown>
 
