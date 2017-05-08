1ef89c23c7c9ea58714de7cb81ffcb0bf2624df7
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-05-07 21:42:26 -0500

    estadisticas corales mun

---
+++ imports/client/estadisticasMun.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {Well, Thumbnail} from 'react-bootstrap';
+import Navigbar from './Navigbar.jsx';
+
+//import manejoRegiones from '../../client/manejoRegiones.js'
+
+export default class problematica extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: '',
+    };
+  }
+
+    render() {
+
+        return (
+
+              <div>
+                <div className="col-md-2"></div>
+                  <div className="col-md-8" ><h2>
+                      Datos sobre los corales en la gran barrera de coral
+                  </h2>
+
+                  <div id="estadisticasMun">
+                  </div></div>
+                  <div className="col-md-2"></div>
+              </div>
+
+        );
+    }
+}
