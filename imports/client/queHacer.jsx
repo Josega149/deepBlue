d1e7656c3106b5a675460ed69c20436679fccae2
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-05-07 21:42:56 -0500

    que hacer frente a la problematica

--- imports/client/queHacer.jsx
+++ imports/client/queHacer.jsx
@@ -20,10 +20,35 @@ export default class queHacer extends Component {
                 <h1>  ¿Cómo ayudar?  </h1>
 
                 <image src="https://static.pexels.com/photos/160203/pexels-photo-160203.jpeg"></image>
+                <h4>Existen múltiples maneras de ayudar a mitigar el daño que están sufriendo los corales marinos. A continuación podrás
+                  ver algunos tips o pasos a seguir que te ayudarán a no apoyar o hacer actividades que fomenten el blanqueamiento de los corales. </h4>
 
-                <h2>Emite menos C02</h2>
+                <h2><strong>Emite menos C02</strong></h2>
+                <h4>La huella ambiental que está dejando el ser humano es altísima y esto no excluye a los ecosistemas.
+                  La contaminación está llegando a un punto de no retorno y es necesario hacer algo al respecto. </h4>
 
-                <h2>Sé un turista responsable</h2>
+                  <div className="row">
+                    <div className="col-md-6"><h3>Usa la bicicleta!</h3>
+                    <div className="imagen" id="bici"></div></div>
+
+                  <div className="col-md-6"><h3>
+                      Dí NO a los combustibles fosiles.</h3>
+                  <div  className="imagen" id="sol"></div></div>
+
+                  <div className="col-md-6"><h3>Lleva tu propia bolsa de tela al mercado</h3>
+                  <div className="imagen" id="bolsas"></div></div>
+
+                <div className="col-md-6"><h3> Elimina el consumo de pitillos. </h3>
+                <div  className="imagen" id="pitillos"></div></div>
+
+
+              </div>
+
+                <h2><strong>Sé un turista responsable</strong></h2>
+
+                <h4> Muchos lugares donde se realiza buceo actualmente alrededor del mundo no cumplen con
+                  las precauciones pertinentes para cuidar de los arrecifes coralinos. A continuación puedes conocer
+                  las razones des esto. </h4>
 
                     <div className="row">
                       <div className="col-md-6"><h3>Evita golpear o tocar un arrecife de coral</h3>
